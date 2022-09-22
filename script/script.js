b79e2d60a0c1638c5b05ae5cc1074419703b039d
Germe Marc Antoine <markus0059@hotmail.fr>
2022-09-22 13:56:16 +0200

    add amazones.html and responsive

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
-const images = document.querySelector('.images');
 
+const cards = document.querySelector(".cards");
 
-const Myimages = [
+const images = [
   "images/amazones/lesamazones-accueil.png",
   "images/amazones/lesamazones-create.png",
-  "images/amazones/lesamazones-all trajets.png",
+  "images/amazones/lesamazones-all-trajets.png",
   "images/amazones/lesamazones-trajet-info.png",
   "images/amazones/lesamazones-trajet.png",
   "images/amazones/lesamazones-chat.png",
@@ -12,17 +12,10 @@ const Myimages = [
   "images/amazones/lesamazones-dashboard2.png",
 ];
 
-// function createCard() {
-//   const amazonesImages = document.createElement('img');
-//   amazonesImages.classList.add('img-site');
-//   images.appendChild(amazonesImages);
-// }
-
-
 function createCard(imageUrl) {
   const card = document.createElement("div");
   card.classList.add("card");
-  images.appendChild(card);
+  cards.appendChild(card);
 
   const cardHeader = document.createElement("div");
   cardHeader.classList.add("card-header");
@@ -34,4 +27,6 @@ function createCard(imageUrl) {
   cardHeader.appendChild(cardImg);
 }
 
-Myimages.forEach((element) => createCard(element));
+/* Step 4: Create a for loop, for each element of the array */
+
+images.forEach((element) => createCard(element));
