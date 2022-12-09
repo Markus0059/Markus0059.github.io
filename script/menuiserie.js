008c335845c8661b11742f28161a87fb13ed2c06
Germe Marc Antoine <markus0059@hotmail.fr>
2022-12-09 19:02:04 +0100

    create dev branch

---
+++ script/menuiserie.js
@@ -0,0 +1,29 @@
+// const cards = document.querySelector(".cards");
+
+
+// const images = [
+//   "images/menuiserie/accueil-menuiserie.jpeg",
+//   "images/menuiserie/login-menuiserie.jpeg",
+//   "images/menuiserie/listing-menuiserie.jpeg",
+//   "images/menuiserie/show-menuiserie.jpeg"
+// ];
+
+// function createCard(imageUrl) {
+
+//   const card = document.createElement("div");
+//   card.classList.add("card");
+//   cards.appendChild(card);
+
+//   // const cardHeader = document.createElement("div");
+//   // cardHeader.classList.add("card-header");
+//   // card.appendChild(cardHeader);
+
+//   const cardImg = document.createElement("div");
+//   cardImg.style.backgroundImage = `url(${imageUrl})`;
+//   cardImg.classList.add("card-img");
+//   card.appendChild(cardImg);
+// }
+
+// /* Step 4: Create a for loop, for each element of the array */
+
+// images.forEach((element) => createCard(element));
