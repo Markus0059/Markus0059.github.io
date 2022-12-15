e8f66804323fbbc1a741bd9878c6ff758f621f5f
Germe Marc Antoine <markus0059@hotmail.fr>
2022-12-15 01:27:58 +0100

    resolve bug

--- script/index.js
+++ script/index.js
@@ -46,12 +46,30 @@ const mySites = [
   "images/amazones/lesamazones-accueil.png",
   "images/menuiserie.jpeg",
   "images/road2wheels.jpeg",
+  "images/wildSupplies.jpeg",
 ]
 
 const myLiens = [
-  "amazones.html",
-  "menuiserie.html",
-  "road2wheels.html",
+  "https://github.com/DonGrosseto/LES_AMAZONES",
+  "https://github.com/markus0059/menuiserie",
+  "https://github.com/Koddak/road2wheels",
+  "https://github.com/WildCodeSchool/noname",
+]
+
+const texts = [
+  "Projet réalisé au Wagon de Lille . Application mobile destinée essentiellement aux femmes afin que celles-ci puisse"
+  + " voyager ensemble en toute sécurité",
+  "Project personnel pour une entreprise de menuiserie familiale. Possibilité à l'administrateur d'ajouter des réalisations de chantier en attribuant une catégorie." +
+  " Par la suite, le client pourra ajouter un commentaire sur une réalisation",
+  " Project réalisé au Wagon de Lille . Application de location de motos entre particulier avec possibilité de localisation. API google map utilisée.",
+  "Project réalisé à la WildCodeSchool . Site Web de vente de biens entre particuliers. Un Compte admin fut créer permettant ainsi de modifier le caroussel et les catégories",
+]
+
+const technos = [
+  "- Ruby / Rails / Bootstrap / JS",
+  "- Ruby / Rails / JS",
+  "- Ruby / Rails / Bootstrap / JS",
+  "- PHP / Twig / JS (project ensuite basculé en Symfony",
 ]
 
 const buttonInAct = document.querySelectorAll('.btn-inact');
@@ -59,23 +77,39 @@ const buttonAct = document.querySelector(".btn-act");
 const button1 = document.getElementById("btn1");
 const button2 = document.getElementById("btn2");
 const button3 = document.getElementById("btn3");
+const button4 = document.getElementById("btn4");
 const button = document.querySelectorAll('.but');
 const imgSite = document.getElementById("image-site");
 const lienSite = document.getElementById("btn-details");
+const textSite = document.getElementById("text-site");
+const technoSite = document.getElementById("text-techno");
 
 
 
 button1.addEventListener('click', changeClass);
 button2.addEventListener('click', changeClass);
 button3.addEventListener('click', changeClass);
+button4.addEventListener('click', changeClass);
 
 button1.site = mySites[0];
 button2.site = mySites[1];
 button3.site = mySites[2];
+button4.site = mySites[3];
 
 button1.lien = myLiens[0];
 button2.lien = myLiens[1];
 button3.lien = myLiens[2];
+button4.lien = myLiens[3];
+
+button1.text = texts[0];
+button2.text = texts[1];
+button3.text = texts[2];
+button4.text = texts[3];
+
+button1.techno = technos[0];
+button2.techno = technos[1];
+button3.techno = technos[2];
+button4.techno = technos[3];
 
 
 
@@ -85,4 +119,6 @@ function changeClass(event) {
   event.target.className = "btn-act";
   imgSite.src = event.target.site;
   lienSite.href = event.target.lien;
+  textSite.textContent = event.target.text;
+  technoSite.textContent = event.target.techno;
 }
